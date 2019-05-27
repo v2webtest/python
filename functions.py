65e7629bf1c824ed03bab5d5576cb7db1029baae
v2webtest <v2webtest@gmail.com>
2019-05-27 18:10:29 +0530

    Interval Test

--- functions.py
+++ functions.py
@@ -4,3 +4,14 @@ def printMe(str):
     return
 
 printMe("Hi!")
+printMe("I'm first call to user defined functions!")
+printMe("I called it again!!")
+
+def changeMe(pList):
+    pList.append([1,2,3,4,5])
+    print("Values inside the function: ", pList)
+    return
+
+pList = [10,20,30,40,50]    
+changeMe(pList)
+print("Value outside the function: ", pList)
